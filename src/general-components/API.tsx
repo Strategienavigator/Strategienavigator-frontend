b40480a71fba4449e07d3f1222c67391d5321671
Nichlas Schipper <Nichlas12323@gmail.com>
2021-07-18 13:53:33 +0200

    API Calls

---
+++ src/general-components/API.tsx
@@ -0,0 +1,46 @@
+interface CallInterface {
+    callData: any
+    success: boolean
+    status: number
+    response: Response
+}
+
+declare type Methods = "GET" | "POST" | "DELETE" | "PUT";
+
+const callAPI = async (URL: string, method: Methods, data?: FormData | Blob | string, token?: string) => {
+    let callURL = process.env.REACT_APP_API + URL;
+
+    // HEADER
+    let headers: HeadersInit = new Headers();
+    headers.append("Accept", "application/json");
+
+    // TOKEN
+    if (token !== undefined) {
+        headers.append("Authorization", "Bearer " + token);
+    }
+
+    // REQUEST-INIT
+    let requestInit: RequestInit = {
+        method: method,
+        headers: headers,
+        body: data
+    }
+
+    // CALL THE API
+    let call = await fetch(callURL, requestInit);
+    let callData = await call.json();
+
+    // BUILD RESPONSE
+    let response: CallInterface = {
+        callData: callData,
+        success: (call.status === 200),
+        status: call.status,
+        response: call
+    }
+
+    return response;
+}
+
+export {
+    callAPI
+};
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,6 +3,8 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         REACT_APP_NAME: string,
-        REACT_APP_API: string
+        REACT_APP_API: string,
+        REACT_APP_CLIENT_ID: number,
+        REACT_APP_CLIENT_SECRET: string
     }
 }
