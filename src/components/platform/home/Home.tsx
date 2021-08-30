35f7ce1f7ce385001a09ddbd4b767be92ecb14ba
Nichlas Schipper <Nichlas12323@gmail.com>
2021-08-30 13:20:06 +0200

    added searchfunction on homepage
    done by @Marce200700
    
    Co-Authored-By: Marce200700 <63236338+Marce200700@users.noreply.github.com>
    Co-Authored-By: Marco Janssen <22887392+ma1160@users.noreply.github.com>

--- src/components/platform/home/Home.tsx
+++ src/components/platform/home/Home.tsx
@@ -4,10 +4,54 @@ import {Col, Form, FormControl, Row} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowsAlt, faChartPie, faSortAmountDownAlt, faThLarge} from "@fortawesome/free-solid-svg-icons";
 
-import "./home.scss";
-import "./home-desk.scss";
+import {compareTwoStrings} from "string-similarity";
 
-export class Home extends Component<any, any> {
+import "./home.scss";
+import "./home-desk.scss"
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
+
+interface HomeState {
+    tools: {
+        name: string,
+        link: string,
+        icon: IconProp
+    }[]
+}
+
+export class Home extends Component<any, HomeState> {
+    private readonly items;
+    private searchMatch = 0.2;
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            tools: [
+                {
+                    name: "Paarweiser Vergleich",
+                    icon: faSortAmountDownAlt,
+                    link: "/pairwise-comparison"
+                },
+                {
+                    name: "SWOT Analyse",
+                    icon: faThLarge,
+                    link: "/swot-analysis"
+                },
+                {
+                    name: "ABC Analyse",
+                    icon: faChartPie,
+                    link: "/abc"
+                },
+                {
+                    name: "Portfolio Analyse",
+                    icon: faArrowsAlt,
+                    link: "/port"
+                }
+            ]
+        }
+
+        this.items = this.state.tools;
+    }
 
     render() {
         return (
@@ -16,47 +60,55 @@ export class Home extends Component<any, any> {
                     <FormControl
                         size={"sm"}
                         type="search"
+                        onChange={(e) => {
+                            let text = e.target.value;
+                            this.search(text);
+                        }}
                         placeholder="Suchen"
                         aria-label="Suchen"
                     />
                 </Form>
 
                 <Row className={"tools"}>
-                    <Col as={Link} className={"tool"} to={"/pairwise-comparison"}>
-                        <div className={"icon"}>
-                            <FontAwesomeIcon icon={faSortAmountDownAlt}/>
-                        </div>
-                        <div className={"text"}>
-                            Paarweiser Vergleich
-                        </div>
-                    </Col>
-                    <Col as={Link} className={"tool"} to={"/swot-analysis"}>
-                        <div className={"icon"}>
-                            <FontAwesomeIcon icon={faThLarge}/>
-                        </div>
-                        <div className={"text"}>
-                            SWOT Analyse
-                        </div>
-                    </Col>
-                    <Col as={Link} className={"tool"} to={"/abc"}>
-                        <div className={"icon"}>
-                            <FontAwesomeIcon icon={faChartPie}/>
-                        </div>
-                        <div className={"text"}>
-                            ABC Analyse
-                        </div>
-                    </Col>
-                    <Col as={Link} className={"tool"} to={"/port"}>
-                        <div className={"icon"}>
-                            <FontAwesomeIcon icon={faArrowsAlt}/>
-                        </div>
-                        <div className={"text"}>
-                            Portfolio Analyse
-                        </div>
-                    </Col>
+                    {(this.state.tools.map(value => {
+                        return (
+                            <Col key={value.name} as={Link} className={"tool"} to={value.link}>
+                                <div className={"icon"}>
+                                    <FontAwesomeIcon icon={value.icon}/>
+                                </div>
+                                <div className={"text"}>
+                                    {value.name}
+                                </div>
+                            </Col>
+                        );
+                    }))}
                 </Row>
             </div>
         );
+    }
+
+    search(searchValue: string) {
+        let items = this.items;
+        let results = [];
+
+        if (searchValue !== "") {
+            for (let i = 0; i < items.length; i++) {
+                let match = compareTwoStrings(searchValue.toLowerCase(), items[i].name.toLowerCase());
+                if (match > this.searchMatch) {
+                    results.push(items[i]);
+                }
+            }
+
+            if (results.length <= 0) {
+                results = this.items;
+            }
+        } else {
+            results = this.items;
+        }
+
+        this.setState({
+            tools: results
+        });
     }
 
 }
