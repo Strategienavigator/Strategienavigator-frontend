430aeea4c3ea0640c1907e278a2e5ea0e2838eaa
Geist5000 <geist5000@gmail.com>
2021-09-08 18:21:49 +0200

    first full functioning prototype. TODO: appearence

--- src/general-components/PaginationFooter/PaginationFooter.tsx
+++ src/general-components/PaginationFooter/PaginationFooter.tsx
@@ -11,12 +11,12 @@ interface PaginationFooterProps {
     /**
      * Welche Seite gerade ausgewählt ist
      *
-     * startet bei 0
+     * startet bei 1
      */
     currentPage: number
     /**
      * callback, wenn eine Seite ausgewählt wurde
-     * @param pageNumber nummer der ausgewählten Seite (startet bei 0)
+     * @param pageNumber nummer der ausgewählten Seite (startet bei 1)
      */
     pageChosen: (pageNumber: number) => void
 
@@ -32,6 +32,10 @@ interface PaginationFooterProps {
 class PaginationFooter extends Component<PaginationFooterProps, {}> {
 
 
+    static defaultProps = {
+        currentPage: 1
+    };
+
     itemClicked(pageNumber: number, e: React.MouseEvent) {
         e.preventDefault();
         if (pageNumber !== this.props.currentPage)
@@ -41,38 +45,37 @@ class PaginationFooter extends Component<PaginationFooterProps, {}> {
     nextClicked = (e: React.MouseEvent) => {
         e.preventDefault();
         this.props.pageChosen(this.props.currentPage + 1)
-    }
+    };
 
     previousClicked = (e: React.MouseEvent) => {
         e.preventDefault();
         this.props.pageChosen(this.props.currentPage - 1)
-    }
+    };
 
     render() {
-        let active = this.props.currentPage;
+                    // clamp active value between 1 and this.props.pageCount
+        let active = this.props.currentPage < 1 ? 1 : (this.props.currentPage > this.props.pageCount ? this.props.pageCount : this.props.currentPage);
         let items = [];
-
-        for (let number = 0; number < 5; number++) {
+                                            // always have at least one page
+        for (let number = 1; number <= (this.props.pageCount < 1 ? 1 : this.props.pageCount); number++) {
             let isActive = number === active;
             items.push(
                 <PageItem key={number} active={isActive} disabled={!isActive && this.props.disabled}
                           onClick={this.itemClicked.bind(this, number)}>
-                    {number + 1}
+                    {number}
                 </PageItem>,
             );
         }
 
         return (
-            <div>
-                <Pagination>
-                    <PageItem key={"previous"} disabled={this.props.currentPage === 0 || this.props.disabled}
-                              onClick={this.previousClicked}>Vorherige</PageItem>
-                    {items}
-                    <PageItem key={"next"}
-                              disabled={this.props.currentPage === this.props.pageCount - 1 || this.props.disabled}
-                              onClick={this.nextClicked}>Nächste</PageItem>
-                </Pagination>
-            </div>);
+            <Pagination className={"paginationContainer"}>
+                <PageItem key={"previous"} disabled={this.props.currentPage === 1 || this.props.disabled}
+                          onClick={this.previousClicked}>Vorherige</PageItem>
+                {items}
+                <PageItem key={"next"}
+                          disabled={this.props.currentPage === this.props.pageCount || this.props.disabled}
+                          onClick={this.nextClicked}>Nächste</PageItem>
+            </Pagination>);
     }
 }
 
---
+++ src/general-components/Tool/Home/SavePagination/SavePagination.tsx
@@ -0,0 +1,136 @@
+import React, {Component, ReactNode} from "react";
+import {SimpleSaveResource} from "../../../Datastructures";
+import {PaginationFooter} from "../../../PaginationFooter/PaginationFooter";
+import {Loader} from "../../../Loader/Loader";
+import {Session} from "../../../Session/Session";
+import {getSaves} from "../../../API/calls/Saves";
+import {ListGroup, ListGroupItem} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {Tool} from "../../Tool";
+
+interface SavePaginationState {
+    saves: Array<Array<SimpleSaveResource>>
+    page: number
+    pageCount: number
+    loading: boolean
+}
+
+interface SavePaginationProps {
+    tool: Tool
+}
+
+class SavePagination extends Component<SavePaginationProps, SavePaginationState> {
+
+    constructor(props: Readonly<SavePaginationProps> | SavePaginationProps);
+    constructor(props: SavePaginationProps, context: any);
+    constructor(props: Readonly<SavePaginationProps> | SavePaginationProps, context?: any) {
+        super(props, context);
+        this.state = {
+            page : 1,
+            saves: new Array<Array<SimpleSaveResource>>(),
+            pageCount: 1,
+            loading:false
+        }
+    }
+
+
+    shouldComponentUpdate (nextProps: Readonly<SavePaginationProps>, nextState: Readonly<SavePaginationState>, nextContext: any): boolean{
+        return true;
+        // return nextState.page !== this.state.page || this.checkIfSavesDiffer(nextState.saves[nextState.page], this.state.saves[nextState.page]);
+    }
+
+    checkIfSavesDiffer(oldSaves: Array<SimpleSaveResource>, newSaves: Array<SimpleSaveResource>): boolean{
+        return oldSaves !== newSaves;
+    }
+
+    private pageChosenCallback = (currentPage: number) => {
+        this.setState({
+            page: currentPage
+        }, () => {
+            this.loadSavesIfNeeded(this.state.page);
+        });
+
+    };
+
+    loadToolSaves = async (page:number = 1) => {
+        if (Session.isLoggedIn()) {
+            let userID = Session.currentUser?.getID() as number;
+
+            this.setState({
+                loading:true
+            });
+            let call = await getSaves(userID, Session.getToken(), this.props.tool.getID(), page);
+            if (call.success && call.callData.data.length > 0) {
+                let saves = call.callData.data;
+                this.updateSaves(call.callData.meta.current_page, saves);
+                this.setState({
+                    pageCount: call.callData.meta.last_page,
+                    loading:false
+                })
+            }
+        }
+    };
+
+
+    private updateSaves(page:number, newSaves:Array<SimpleSaveResource>){
+        this.setState((s) => {
+            let newState = {saves:s.saves.slice()};
+            newState.saves[page] = newSaves;
+            return newState;
+        });
+    }
+
+    private loadSavesIfNeeded(page:number = 1){
+        if(this.state.saves[page] === null || this.state.saves[page] === undefined){
+            this.loadToolSaves(page);
+        }
+    }
+
+
+    componentDidMount(): void {
+        this.loadToolSaves();
+    }
+
+    render(): ReactNode {
+        console.log(this.state);
+        console.log(!this.state.loading);
+        return (
+            <div>
+                <Loader payload={[]} loaded={(!this.state.loading)}>
+
+                    <ListGroup>
+                        {this.state.saves[this.state.page]?.length <= 0 && (
+                            <ListGroupItem>Sie haben aktuell keine Speicherstände.</ListGroupItem>
+                        )}
+
+                        {this.state.saves[this.state.page]?.map(value => {
+                            let save = value;
+                            return (
+                                <ListGroupItem as={Link} to={this.props.tool?.getLink() + "/" + save.id}
+                                               key={save.id}
+                                               action>
+                                    {save.name}
+                                </ListGroupItem>
+                            );
+                        })}
+
+                    </ListGroup>
+
+                </Loader>
+                <PaginationFooter pageCount={this.state.pageCount} pageChosen={this.pageChosenCallback} currentPage={this.state.page} disabled={this.state.loading}/>
+            </div>
+        );
+    }
+
+
+}
+
+
+export {
+    SavePagination
+}
+
+export type {
+    SavePaginationProps,
+    SavePaginationState
+}
