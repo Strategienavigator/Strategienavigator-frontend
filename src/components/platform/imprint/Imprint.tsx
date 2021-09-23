13084cf02388ab96c4f8ba6611b26955e99b0c6a
Geist5000 <geist5000@gmail.com>
2021-09-23 20:03:56 +0200

    added imprint and data privacy notices

--- src/components/platform/imprint/Imprint.tsx
+++ src/components/platform/imprint/Imprint.tsx
@@ -1,12 +1,92 @@
 import {Component} from "react";
 
 import "./imprint.scss";
+import {Container} from "react-bootstrap";
 
 
 export class Imprint extends Component<any, any> {
 
     render() {
-        return "Impressum";
+        return (<>
+                <h1>Impressum</h1>
+                <p> Dies ist ein Projekt der Jade Hochschule Wilhelmshaven/Oldenburg/Elsfleth<br/>
+                    Für weitere Information siehe <a href={"https://www.jade-hs.de/impressum"} rel={"noreferrer"} target={"_blank"}>Impressum der Jade Hochschule</a></p>
+                <p><strong>Name und Kontaktdaten der Verantwortlichen (auch für Datenschutz):</strong>
+                    <ul>
+                        <li>Prof. Christian Gündling</li>
+                        <li>Rico Meiner</li>
+                    </ul>
+                    </p>
+                <p><strong>Adresse:</strong><br/>Friedrich Paffrath Straße 101<br/>26389
+                    Wilhelmshaven</p>
+                <p><strong>Telefon:</strong> 04421 9852424<br/>
+                    <strong>E-Mail: </strong><a
+                        href="mailto:strategie-navigator@jade-hs.de?subject=Strategienavigator%20Impressum">strategie-navigator@jade-hs.de</a>
+                    <br/>
+
+                    <br/><br/>
+                </p>
+                <h2>Disclaimer – rechtliche Hinweise</h2>
+                <p>§ 1 Warnhinweis zu Inhalten<br/> Die kostenlosen und frei zugänglichen Inhalte
+                    dieser Anwendung wurden mit größtmöglicher Sorgfalt erstellt. Der Anbieter dieser Anwendung
+                    übernimmt
+                    jedoch keine
+                    Gewähr für die Richtigkeit und Aktualität der bereitgestellten kostenlosen und frei zugänglichen
+                    journalistischen
+                    Ratgeber und Nachrichten. Namentlich gekennzeichnete Beiträge geben die Meinung des jeweiligen
+                    Autors
+                    und nicht
+                    immer die Meinung des Anbieters wieder. Allein durch den Aufruf der kostenlosen und frei
+                    zugänglichen
+                    Inhalte
+                    kommt
+                    keinerlei Vertragsverhältnis zwischen dem Nutzer und dem Anbieter zustande, insoweit fehlt es am
+                    Rechtsbindungswillen des Anbieters.<br/><br/>
+                    § 2 Externe Links<br/>
+                    Diese Anwendung enthält Verknüpfungen zu Websites Dritter ("externe Links"). Diese Websites
+                    unterliegen der
+                    Haftung
+                    der jeweiligen Betreiber. Der Anbieter hat bei der erstmaligen Verknüpfung der externen Links die
+                    fremden
+                    Inhalte
+                    daraufhin überprüft, ob etwaige Rechtsverstöße bestehen. Zu dem Zeitpunkt waren keine Rechtsverstöße
+                    ersichtlich.
+                    Der Anbieter hat keinerlei Einfluss auf die aktuelle und zukünftige Gestaltung und auf die Inhalte
+                    der
+                    verknüpften
+                    Seiten. Das Setzen von externen Links bedeutet nicht, dass sich der Anbieter die hinter dem Verweis
+                    oder Link
+                    liegenden Inhalte zu Eigen macht. Eine ständige Kontrolle der externen Links ist für den Anbieter
+                    ohne konkrete
+                    Hinweise auf Rechtsverstöße nicht zumutbar. Bei Kenntnis von Rechtsverstößen werden jedoch derartige
+                    externe
+                    Links unverzüglich gelöscht.<br/><br/>
+                    § 3 Urheber- und Leistungsschutzrechte<br/>
+                    Die in dieser Anwendung veröffentlichten Inhalte unterliegen dem deutschen Urheber- und
+                    Leistungsschutzrecht. Jede vom deutschen Urheber- und Leistungsschutzrecht nicht zugelassene
+                    Verwertung bedarf
+                    der vorherigen schriftlichen Zustimmung des Anbieters oder jeweiligen Rechteinhabers. Dies gilt
+                    insbesondere für
+                    Vervielfältigung, Bearbeitung, Übersetzung, Einspeicherung, Verarbeitung bzw. Wiedergabe von
+                    Inhalten in
+                    Datenbanken oder anderen elektronischen Medien und Systemen. Inhalte und Rechte Dritter sind dabei
+                    als solche
+                    gekennzeichnet.
+                    Die unerlaubte Vervielfältigung oder Weitergabe einzelner Inhalte oder kompletter Seiten ist nicht
+                    gestattet und
+                    strafbar. Lediglich die Herstellung von Kopien und Downloads für den persönlichen, privaten und
+                    nicht
+                    kommerziellen Gebrauch ist erlaubt.<br/><br/>
+                    § 4 Besondere
+                    Nutzungsbedingungen<br/>
+                    Soweit besondere Bedingungen für einzelne Nutzungen dieser Anwendung von den
+                    vorgenannten Paragraphen abweichen, wird an entsprechender Stelle ausdrücklich darauf hingewiesen.
+                    In diesem
+                    Falle gelten im jeweiligen Einzelfall die besonderen Nutzungsbedingungen.</p>
+                <p>Quelle: <a href={"https://www.juraforum.de"}>Zur Quelle (Bearbeitet)</a></p>
+            </>
+        );
+
     }
 
 }
