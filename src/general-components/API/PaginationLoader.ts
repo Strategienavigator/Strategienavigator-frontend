3a91e2e892c6b6197e448eebaf3db3dfe6890738
Geist5000 <geist5000@gmail.com>
2021-09-30 13:16:35 +0200

    removed possible getPage function of pagination loader

--- src/general-components/API/PaginationLoader.ts
+++ src/general-components/API/PaginationLoader.ts
@@ -4,54 +4,38 @@ import {CallInterface} from "./API";
 /**
  * Klasse welche das Laden der Seiten einer Pagination übernimmt. Die Ergebnisse werden gecached.
  *
- * Man kann der Klasse entweder eine function im Konstruktor übergeben, welche die Daten vom Backend lädt
- * oder man erbt aus der Klasse und überschreibt die {@link loadPage} Funktion
+ * Man muss der Klasse eine function im Konstruktor übergeben, welche die Daten vom Backend lädt
  */
 export class PaginationLoader<D extends object> {
     private readonly data: Array<Array<D>>;
     private readonly getPageCallback?: (page: number) => Promise<CallInterface<PaginationResource<D>> | null>;
-    private callbackGiven: boolean = false;
     private _pageCount: number = 0;
 
     constructor(cb?: (page: number) => Promise<CallInterface<PaginationResource<D>> | null>) {
         this.data = [];
-        this.callbackGiven = cb !== null && cb !== undefined;
-
         this.getPageCallback = cb;
-
     }
-
-    protected async loadPage(page: number): Promise<CallInterface<PaginationResource<D>> | null> {
-        return null;
-    };
 
     public async getPage(page: number) {
         if (this.getPageData(page) === null || this.getPageData(page) === undefined) {
-            let result = await this.getGetPageCallback()(page);
-            if (result === null || result === undefined) {
-                if (!this.callbackGiven) {
-                    throw new Error("No PageCallback given!");
+            if (this.getPageCallback) {
+                let result = await this.getPageCallback(page);
+                if (result !== null && result !== undefined) {
+                    if (result.success) {
+                        let d = result.callData;
+                        this.setPageData(d.meta.current_page, d.data);
+                        this.pageCount = d.meta.last_page;
+                        return this.getPageData(d.meta.current_page);
+                    }
                 }
             } else {
-                if (result.success) {
-                    let d = result.callData;
-                    this.setPageData(d.meta.current_page, d.data);
-                    this.pageCount = d.meta.last_page;
-                    return this.getPageData(d.meta.current_page);
-                }
+                throw new Error("No PageCallback given!");
             }
+
         } else {
             return this.getPageData(page);
         }
         return null;
-    }
-
-    private getGetPageCallback() {
-        if (this.callbackGiven) {
-            return this.getPageCallback as (page: number) => Promise<CallInterface<PaginationResource<D>> | null>;
-        } else {
-            return this.loadPage.bind(this);
-        }
     }
 
 
