b908e16c43bb49672e22f4daf0449d958e048917
Geist5000 <geist5000@gmail.com>
2021-10-22 16:15:42 +0200

    created settingscache class

---
+++ src/general-components/API/SettingsCache.ts
@@ -0,0 +1,93 @@
+import {PaginationLoader} from "./PaginationLoader";
+import {SettingResource, UserSettingResource} from "../Datastructures";
+import * as Settings from "../../general-components/API/calls/Settings";
+
+export interface UserSetting extends SettingResource {
+    value: string
+}
+
+
+/**
+ * Speichert alle Einstellungsmöglichkeiten und aktualisiert diese regelmäßig
+ */
+export class SettingsCache {
+
+
+    private _userId: number;
+    /**
+     * last time settings did get updated
+     * @private
+     */
+    private _lastLoad: Date;
+    /**
+     * all settings with the current value
+     * @private
+     */
+    private _userSettings: UserSetting[];
+
+    private settingsLoader: PaginationLoader<SettingResource>;
+    private userSettingsLoader: PaginationLoader<UserSettingResource>;
+
+    constructor(token: string, userId: number) {
+        this._userId = userId;
+        this._lastLoad = new Date(0);
+        this._userSettings = [];
+
+        this.settingsLoader = new PaginationLoader<SettingResource>((page) => {
+            return Settings.getSettings(token, page)
+        })
+        this.userSettingsLoader = new PaginationLoader<UserSettingResource>((page) => {
+            return Settings.getUserSettings(userId, token, page)
+        })
+
+    }
+
+    private async loadData() {
+        let settings = await this.settingsLoader.getAll();
+        let userSettings = await this.userSettingsLoader.getAll();
+        return settings.map((s) => {
+            let userS = userSettings.find((su) => su.setting_id === s.id);
+            let v = s.default;
+            if (userS && userS.value && userS.value.length > 0)
+                v = userS.value;
+            return {...s, value: v} as UserSetting;
+        })
+    }
+
+    public async updateData() {
+        this._userSettings = await this.loadData();
+        this._lastLoad = new Date();
+    }
+
+
+    get lastLoad(): Date {
+        return this._lastLoad;
+    }
+
+    private set lastLoad(value: Date) {
+        this._lastLoad = value;
+    }
+
+
+    get userId(): number {
+        return this._userId;
+    }
+
+    private set userId(value: number) {
+        this._userId = value;
+    }
+
+    get userSettings(): UserSetting[] {
+        return this._userSettings;
+    }
+
+    private set userSettings(value: UserSetting[]) {
+        this._userSettings = value;
+    }
+
+    public shouldUpdate(){
+        let today = new Date();
+        let diff = today.getTime() - this.lastLoad.getTime()
+        return diff > 300000// 1000*60*5 5min
+    }
+}
