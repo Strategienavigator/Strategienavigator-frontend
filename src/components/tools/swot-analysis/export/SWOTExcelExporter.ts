d8d5c266fbddebbae79485e2601dfa6c2564e534
Geist5000 <geist5000@gmail.com>
2021-11-25 13:59:52 +0100

    created basic classes to generate excel files

--- src/components/tools/swot-analysis/export/SWOTExcelExporter.ts
+++ src/components/tools/swot-analysis/export/SWOTExcelExporter.ts
@@ -3,8 +3,12 @@ import {SWOTAnalysisValues} from "../SWOTAnalysis";
 import {SaveResource} from "../../../../general-components/Datastructures";
 
 class SWOTExcelExporter extends ExcelExporter<SWOTAnalysisValues>{
-    protected buildExcel(data: SaveResource<SWOTAnalysisValues>): object {
-        return {};
+    protected buildExcel(workbook: XLSX.WorkBook, data: SaveResource<SWOTAnalysisValues>): boolean {
+
+        let ws1 = this.addSheet("peter");
+
+        var new_workbook = XLSX.utils.book_new();
+        return true;
     }
 }
 
--- src/general-components/Export/ExcelExporter.tsx
+++ src/general-components/Export/ExcelExporter.tsx
@@ -1,8 +1,11 @@
 import {Exporter} from "./Exporter";
 import {SaveResource} from "../Datastructures";
+import XLSX,{WorkBook,Sheet,WorkSheet} from "xlsx";
+import React from "react";
 
 
 abstract class ExcelExporter<D> extends Exporter<D> {
+    private sheets: Array<XLSX.Sheet> = new Array<XLSX.Sheet>();
 
     constructor() {
         super("Excel", "xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
@@ -13,7 +16,14 @@ abstract class ExcelExporter<D> extends Exporter<D> {
         return [];
     }
 
-    protected abstract buildExcel(data: SaveResource<D>): object;
+    protected abstract buildExcel(workbook: WorkBook, data: SaveResource<D>): boolean;
+
+    protected addSheet(name:string){
+        // TODO: HTML tabellen umbauen in sheets!
+        //  Dann die JSX-Tabelle umbauen als string und diesen mittels XLSX.read einbinden
+        //  NOTE: XLSX.read can handle HTML represented as strings
+        //  ALSO: XLSX.utils.json_to_sheet(csvData);
+    }
 
 }
 export {
