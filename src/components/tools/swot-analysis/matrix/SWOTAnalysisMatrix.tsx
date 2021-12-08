6ff3ebd5c7b89a0281ec0496b3485e8260499201
Gomanini <goman-muestak@hotmail.de>
2021-12-08 10:24:31 +0100

    Update SWOTAnalysisMatrix.tsx
    All steps commented

--- src/components/tools/swot-analysis/matrix/SWOTAnalysisMatrix.tsx
+++ src/components/tools/swot-analysis/matrix/SWOTAnalysisMatrix.tsx
@@ -6,58 +6,64 @@ import Beispiel from "./beispiel.png";
 
 class SWOTAnalysisMatrix extends MatrixComponent<any> {
 
-    render() {
+    render() {                             
         console.log(this.getData());
+         
+        //Hier entstehen die Punkte für die Chancen
         let headernumber = [];
-        let numberLength = 5;
+        let numberLength = 7;
         for (let i = 0; i < numberLength; i++) {
             headernumber[i]= <div>{i +1}</div>;
         }
-
+        
+        //Hier entstehen die Punkte für die Stärken
         let headerCapitals = [];
         let headerCapitalLength = 5;
         for (let i = 0; i < headerCapitalLength; i++) {
             headerCapitals[i] = <div>{String.fromCharCode(65 + i)}</div>;
         }
-
+        
+        //Hier entstehen die Punkte für die Schwächen
         let leftLetters = [];
         let leftLettersLength = 5;
         for (let i = 0; i < leftLettersLength; i++) {
             leftLetters[i] = <div>{String.fromCharCode(97 + i)}</div>;
         }
         
+        //Hier entstehen die Punkte für die Risiken
         let headerRomanCapitals = [];
         let headerRomanCapitalsLength = 5;
         let romanCapitals = ["I","II","III","IV","V","VI","VII","VIII","IX"];
         for (let i = 0; i < headerRomanCapitalsLength; i ++){
             headerRomanCapitals[i] = <div>{romanCapitals[i]}</div>;
         }
-
+        
+        //Hier entstehen die Kombinationen zwischen Chancen und Stärken
         let bodyone = [];
         let bodyoneLength = numberLength * headerCapitalLength;
         for (let i = 0; i < bodyoneLength; i++) {
             bodyone[i] = <div></div>;
-            
         }
-
+        
+        //Hier entstehen die Kombinationen zwischen Risiken und Stärken
         let bodytwo = [];
         let bodytwoLength = headerRomanCapitalsLength * headerCapitalLength;
         for (let i = 0; i < bodytwoLength; i++) {
             bodytwo[i] = <div></div>;
-            
         }
         
+        //Hier entstehen die Kombinationen zwischen Chancen und Schwächen
         let bodythree = [];
         let bodythreeLength = numberLength * leftLettersLength;
         for (let i = 0; i <  bodythreeLength; i++) {
             bodythree[i] = <div></div>;
-            
         }
+        
+        //Hier entstehen die Kombinationen zwischen Risiken und Schwächen
         let bodyfour = [];
         let bodyfourLength = headerRomanCapitalsLength * leftLettersLength;
         for (let i = 0; i < bodyfourLength; i++) {
             bodyfour[i] = <div></div>;
-            
         }
         
         return (
