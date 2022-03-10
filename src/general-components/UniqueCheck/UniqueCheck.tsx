a6efdc1512744583de7670f502bee380c629f790
Geist5000 <geist5000@gmail.com>
2022-03-10 17:40:22 +0100

    included feedback of code reviews

--- src/general-components/UniqueCheck/UniqueCheck.tsx
+++ src/general-components/UniqueCheck/UniqueCheck.tsx
@@ -4,7 +4,6 @@ import {Form, FormControlProps} from "react-bootstrap";
 import {Omit, ReplaceProps} from "react-bootstrap/helpers";
 import {APIArgs, CallInterface} from "../API/API";
 import {faTimes} from "@fortawesome/free-solid-svg-icons/";
-import {faCheck} from "@fortawesome/free-solid-svg-icons";
 import {Loader} from "../Loader/Loader";
 
 import "./unique-check.scss";
@@ -13,15 +12,40 @@ import FAE from "../Icons/FAE";
 
 
 export interface UniqueCheckProps {
+    /**
+     * Methode die ein Network Request schicken sollte, welcher mit Informationen über die Verfügbarkeit der Ressource antworten sollte.
+     */
     callback?: ((input: string, apiArgs?: APIArgs) => Promise<CallInterface<DefaultResponse<AvailabilityCheckResource>> | null>)
+    /**
+     * Name der Ressource die dem User angezeigt wird
+     *
+     * Sollte die Einzahl sein
+     */
     entityName: string
+    /**
+     * Ob Fehler angezeigt werden sollen
+     */
     suppressErrors: boolean
 }
 
 export interface UniqueCheckState {
+    /**
+     * Ob der Netzwerk Request gerade lädt
+     */
     isLoading: boolean
+    /**
+     * Ob der Netzwerk Request erfolgreich war
+     *
+     * wenn noch kein Request abgeschickt wurde oder wenn fehler aufgetreten ist das Feld undefined
+     */
     success?: boolean
+    /**
+     * Grund wieso die Resource nicht mehr verfügbar ist
+     */
     reason?: "taken" | "blocked" | "invalid"
+    /**
+     * Ob beim letzten Request ein Fehler aufgetreten ist
+     */
     error: boolean
 }
 
@@ -42,26 +66,50 @@ export class UniqueCheck extends Component<ReplaceProps<"input", FormControlProp
     }
 
 
+    /**
+     * Gibt einen Text zurück das ein fehler aufgetreten ist
+     * @private
+     */
     private getFailMessage() {
         return "Die Verfügbarkeit konnte nicht überprüft werden.";
     }
 
+    /**
+     * Gibt einen Text zurück das die untersuchte Resource nicht verfügbar ist
+     * @private
+     */
     private getTakenMessage() {
         return this.props.entityName + " ist nicht Verfügbar!";
     }
 
+    /**
+     * Gibt einen Text zurück das die untersuchte Resource verfügbar ist.
+     * @private
+     */
     private getAvailableMessage() {
         return this.props.entityName + " ist Verfügbar!";
     }
 
+    /**
+     * Gibt einen Text zurück das die untersuchte Resource kein richtiges format aufweist
+     * @private
+     */
     private getInvalidMessage() {
         return this.props.entityName + " ist nicht korrekt!";
     }
 
+    /**
+     * Gibt ein Text zurück das die untersuchte Resource vom Server nicht erlaubt wird.
+     * @private
+     */
     private getBlockedMessage() {
         return this.props.entityName + " ist nicht erlaubt!";
     }
 
+    /**
+     * Gibt den Text zurück, welcher das letzte Ergebniss der Verfügbarkeitsprüfung beschreibt.
+     * @private
+     */
     private getMessage() {
         if (this.state.success) {
             return this.getAvailableMessage();
@@ -91,6 +139,9 @@ export class UniqueCheck extends Component<ReplaceProps<"input", FormControlProp
     }
 
 
+    /**
+     * Gibt ein Element zurück, welches anzeigt, ob es Fehler gibt oder ob die Anfrage erfolgreich war
+     */
     renderErrors = () => {
         return (<>
             {(!this.state.isLoading && this.state.error) && (
@@ -139,6 +190,12 @@ export class UniqueCheck extends Component<ReplaceProps<"input", FormControlProp
     }
 
 
+    /**
+     * Funktion welche ausgeführt wird, wenn sich der Wert des Input-Tags ändert.
+     *
+     * Nach kurzer Zeit ohne eingaben, wird ein Request an das Backend geschickt, in dem
+     * @param e
+     */
     changed = async (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         if (this.timeout) {
             clearTimeout(this.timeout);
@@ -172,7 +229,9 @@ export class UniqueCheck extends Component<ReplaceProps<"input", FormControlProp
                         return;
                     }
                 } catch (reason) {
-                    console.dir(reason);
+                    if(process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'development'){
+                        console.error(reason);
+                    }
                 }
 
 
