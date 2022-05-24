9a4131a6e2091cf6dfa062b64cb6942444f98ec3
Geist5000 <geist5000@gmail.com>
2022-05-24 14:01:03 +0200

    fixed mysql connection of cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,22 +12,20 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 const ms = require('smtp-tester');
-
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
 
     // Usage: cy.task('queryDb', query)
-   
 
 
     if (config.testingType === 'component') {
         require('@cypress/react/plugins/react-scripts')(on, config)
-    }else{
+    } else {
         // starts the SMTP server at localhost:7777
         const port = 7777;
         const mailServer = ms.init(port)
@@ -51,28 +49,35 @@ module.exports = (on, config) => {
 
 
         const mysql = require("mysql");
+
         function queryTestDb(query, config) {
-        // creates a new mysql connection using credentials from cypress.json env's
-        const connection = mysql.createConnection(config.env.db);
-        // start connection to db
-        connection.connect();
-        // exec query + disconnect to db as a Promise
-        return new Promise((resolve, reject) => {
-            connection.query(query, (error, results) => {
-            if (error) reject(error);
-            else {
-                connection.end();
-                // console.log(results)
-                return resolve(results);
-            }
+            // creates a new mysql connection using credentials from cypress.json env's
+            const connection = mysql.createConnection({
+                "host": config.env.DB_HOST,
+                "user": config.env.DB_USER,
+                "password": config.env.DB_PASSWORD,
+                "port":config.env.DB_PORT
             });
-        });
+            // start connection to db
+            connection.connect();
+            // exec query + disconnect to db as a Promise
+            return new Promise((resolve, reject) => {
+                connection.query(query, (error, results) => {
+                    if (error) reject(error);
+                    else {
+                        connection.end();
+                        // console.log(results)
+                        return resolve(results);
+                    }
+                });
+            });
         }
+
         on("task", {
             queryDb: query => {
-              return queryTestDb(query, config);
+                return queryTestDb(query, config);
             }
-          });
+        });
     }
 
     return require('cypress-local-config')(config);
