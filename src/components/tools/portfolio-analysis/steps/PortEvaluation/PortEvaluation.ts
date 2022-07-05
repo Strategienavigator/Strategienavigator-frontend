c695a766c573c7cbb3372a202ff8b6a54b8fbca5
Nichlas Schipper <Nichlas12323@gmail.com>
2022-07-05 19:57:11 +0200

    Step 4 Portfolio Analysis - Evaluation
    Co-Authored-By: Marco Janssen <22887392+ma1160@users.noreply.github.com>

--- src/components/tools/portfolio-analysis/steps/PortEvaluation/PortEvaluation.ts
+++ src/components/tools/portfolio-analysis/steps/PortEvaluation/PortEvaluation.ts
@@ -7,10 +7,19 @@ import {PortfolioAnalysisValues} from "../../PortfolioAnalysis";
 import {StepProp} from "../../../../../general-components/Tool/SteppableTool/StepComponent/Step/Step";
 import {Draft} from "immer";
 import {UIError} from "../../../../../general-components/Error/UIErrors/UIError";
-import {PortEvaluationComponent} from "./PortEvaluationComponent";
+import {PortEvaluationComponent, PortEvaluationValues} from "./PortEvaluationComponent";
+import {WritableDraft} from "immer/dist/internal";
+import {
+    CompareComponentValues,
+    CompareValue
+} from "../../../../../general-components/CompareComponent/CompareComponent";
+import {CompareHeader} from "../../../../../general-components/CompareComponent/Header/CompareHeaderAdapter";
+import {CompareSymbolHeader} from "../../../../../general-components/CompareComponent/Header/CompareSymbolHeader";
 
 
 export class PortEvaluation implements StepDefinition<PortfolioAnalysisValues>, StepDataHandler<PortfolioAnalysisValues> {
+    public static header = new CompareSymbolHeader(["--", "-", "0", "+", "++"]);
+
     dataHandler: StepDataHandler<PortfolioAnalysisValues>;
     form: FunctionComponent<StepProp<PortfolioAnalysisValues>> | ComponentClass<StepProp<PortfolioAnalysisValues>>;
     id: string;
@@ -28,15 +37,71 @@ export class PortEvaluation implements StepDefinition<PortfolioAnalysisValues>, 
     }
 
     fillFromPreviousValues(data: Draft<PortfolioAnalysisValues>): void {
+        let criterias = data["port-criterias"];
+        let objects = data["port-objects"];
+
+        if (criterias !== undefined && objects !== undefined) {
+            let allCriterias = criterias["attractivity"].concat(criterias["comp-standing"]);
+            let evaluations: any[] = [];
+
+            let compareValues = objects.objects.map(() => {
+               return {
+                 value: null,
+                 header: null
+               };
+            });
+
+            for (let i = 0; i < allCriterias.length; i++) {
+                evaluations.push({
+                    criteriaIndex: i,
+                    rating: {
+                        comparisons: compareValues,
+                        headers: PortEvaluation.header.getHeaders()
+                    }
+                });
+            }
+
+            data["port-evaluation"] = {
+                evaluation: evaluations
+            };
+        }
     }
 
     isUnlocked(data: PortfolioAnalysisValues): boolean {
-        return false;
+        return data["port-evaluation"] !== undefined;
     }
 
-
     validateData(data: PortfolioAnalysisValues): UIError[] {
-        return [];
+        let errors: UIError[] = [];
+        let evaluation = data["port-evaluation"];
+
+        if(evaluation !== undefined) {
+            let errorFound = false;
+            let i = 0;
+            while(!errorFound && i < evaluation.evaluation.length) {
+                let e = 0;
+                while (!errorFound && e < evaluation.evaluation[i].rating.comparisons.length) {
+                    let value = evaluation.evaluation[i].rating.comparisons[e].value;
+                    if (value === null || value === "") {
+                        errorFound = true;
+                    }
+                    e++;
+                }
+                i++;
+            }
+            if(errorFound) {
+                errors.push(
+                    {
+                        id: "port-evaluation.empty",
+                        message: "Es müssen alle Kriterien bewertet werden!",
+                        level: "error"
+                    }
+                )
+            }
+        }
+
+
+        return errors;
     }
 
 }
--- src/components/tools/portfolio-analysis/steps/PortEvaluation/PortEvaluationComponent.tsx
+++ src/components/tools/portfolio-analysis/steps/PortEvaluation/PortEvaluationComponent.tsx
@@ -1,23 +1,131 @@
-import {Step, StepProp} from "../../../../../general-components/Tool/SteppableTool/StepComponent/Step/Step";
+import {
+    shallowCompareStepProps,
+    Step,
+    StepProp
+} from "../../../../../general-components/Tool/SteppableTool/StepComponent/Step/Step";
 import {PortfolioAnalysisValues} from "../../PortfolioAnalysis";
+import {CardComponentFields} from "../../../../../general-components/CardComponent/CardComponent";
+import {isDesktop} from "../../../../../general-components/Desktop";
+import {
+    CompareComponent,
+    CompareComponentValues
+} from "../../../../../general-components/CompareComponent/CompareComponent";
+import {LinearCardComponentFieldsAdapter} from "../../../../../general-components/CompareComponent/Adapter/LinearCardComponentFieldsAdapter";
+import {PortEvaluation} from "./PortEvaluation";
+import {UIErrorBanner} from "../../../../../general-components/Error/UIErrors/UIErrorBannerComponent/UIErrorBanner";
+import {Accordion} from "react-bootstrap";
 
 
 interface PortEvaluationValues {
-
+    evaluation: {
+        criteriaIndex: number,
+        rating: CompareComponentValues
+    }[]
 }
 
 class PortEvaluationComponent extends Step<PortfolioAnalysisValues, {}> {
+    private index: number = -1;
 
     public constructor(props: StepProp<PortfolioAnalysisValues>, context: any) {
         super(props, context);
     }
 
     shouldComponentUpdate(nextProps: Readonly<StepProp<PortfolioAnalysisValues>>, nextState: Readonly<{}>, nextContext: any): boolean {
-        return true;
+        return !shallowCompareStepProps(this.props, nextProps, (oldData, newData) => {
+            return oldData["port-evaluation"] === newData["port-evaluation"];
+        });
     }
 
     build(): JSX.Element {
-        return <></>;
+        this.index = -1;
+        return (
+          <>
+              {(isDesktop()) ? this.desktop() : this.mobile()}
+
+              <UIErrorBanner id={"port-evaluation.empty"} />
+          </>
+        );
+    }
+
+    getCompareComponents(type: "attractivity" | "comp-standing"): JSX.Element[] {
+        let allValues = this.props.save.data["port-evaluation"]?.evaluation;
+        let allCriterias = this.props.save.data["port-criterias"];
+        let objects = this.props.save.data["port-objects"];
+        let criterias: CardComponentFields;
+
+        if (allCriterias?.attractivity !== undefined && allCriterias["comp-standing"] !== undefined && objects !== undefined) {
+            if (type === "attractivity") {
+                criterias = allCriterias?.attractivity;
+            } else {
+                criterias = allCriterias?.["comp-standing"];
+            }
+
+            let adapter = new LinearCardComponentFieldsAdapter(objects.objects);
+
+            return criterias.map((item) => {
+                if (allValues) {
+                    this.index++;
+                    let localIndex = this.index; // Muss gemacht werden, weil sonst this.index als referenz genommen wird. Hier muss aber ein eigener int vorhanden sein
+                    let values = allValues[this.index].rating;
+
+                    return (
+                        <div className={"evaluation"}>
+                            <h6>{item.name}</h6>
+
+                            <CompareComponent
+                                name={type + "-" + localIndex}
+                                header={PortEvaluation.header}
+                                fields={adapter}
+                                values={values}
+                                disabled={this.props.disabled}
+                                onChanged={(values) => {
+                                    this.valuesChanged(values, localIndex);
+                                }}
+                            />
+                        </div>
+                    );
+                }
+                return <></>;
+            });
+        }
+        return [];
+    }
+
+    desktop(): JSX.Element {
+        return (
+            <>
+                <h6 className={"compare-header"}>Marktattraktivität</h6>
+                {this.getCompareComponents("attractivity")}
+
+                <h6 className={"compare-header"}>Wettbewerbsposition</h6>
+                {this.getCompareComponents("comp-standing")}
+            </>
+        );
+    }
+
+    mobile(): JSX.Element {
+        return (
+            <>
+                <Accordion defaultActiveKey={"attractivity"}>
+                    <Accordion.Item eventKey={"attractivity"}>
+                        <Accordion.Header>Marktattraktivität</Accordion.Header>
+                        <Accordion.Body>{this.getCompareComponents("attractivity")}</Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey={"comp-standing"}>
+                        <Accordion.Header>Wettbewerbsposition</Accordion.Header>
+                        <Accordion.Body>{this.getCompareComponents("comp-standing")}</Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </>
+        );
+    }
+
+    valuesChanged = (values: CompareComponentValues, index: number) => {
+        this.props.saveController.onChanged((save) => {
+            if (save.data["port-evaluation"] !== undefined) {
+                save.data["port-evaluation"].evaluation[index].rating = values;
+            }
+        });
     }
 
 }
