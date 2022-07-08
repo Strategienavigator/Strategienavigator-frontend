d0d504f497c44901125d263d9bf900aed23c188c
Maruun86 <pierrea@web.de>
2022-07-08 08:51:14 +0200

    SWOT Analysis optimization
    More optimizations

--- cypress/integration/tool_tests/swot-analysis.js
+++ cypress/integration/tool_tests/swot-analysis.js
@@ -47,51 +47,69 @@ describe('SWOT Analyisis', () => {
               statusCode: 201
             })
           cy.log("new SWOT created and saved for anonymous")
-      })
+          cy.log("Removing DB entry for testingpurposes")
+          cy.task("queryDb",`DELETE FROM toolbox.saves WHERE name="TEST-SWOT";`);
+        })
 
-      it.only('trys to create a new SWOT as max@test.test and fill in Factors', () =>{
-        cy.visit("/")  
-        cy.loginViaApi("max@test.test", "password")
-        cy.visit("/swot-analysis")
-        cy.url().should("include","swot-analysis")
+        it('trys to create a new SWOT as max@test.test and fill in Factors', () =>{
+          cy.visit("/")  
+          cy.loginViaApi("max@test.test", "password")
+          cy.visit("/swot-analysis")
+          cy.url().should("include","swot-analysis")
 
-        cy.contains("Neue Analyse")
-        .click()
-        cy.url().should("include", "swot-analysis/new")
-        
-        cy.get("input[id='name']")
-        .clear()
-        .type("TEST-SWOT VON MAX")
-        cy.get("textarea[id='description']")
-        .clear()
-        .type("TEST-SWOT ist ein Testscenario von einem API eingeloggten benutzer")
-        
-        cy.intercept('POST', /.*api\/saves.*/).as('save')
-        cy.get('button[type="submit"]')
-        .click()
-        cy.wait("@save")
-        cy.get("@save")
-        .its("response")
-        .should('include',{
-            statusCode: 201
-          })
-        cy.log("new SWOT created and saved for max@test.test")
+          cy.contains("Neue Analyse")
+          .click()
+          cy.url().should("include", "swot-analysis/new")
           
-        
-        
-        //--
-        //Fügen jeweils 2 einträge bei den SWOT hinzu
-        cy.get('div[class="addCard card"]')
-          .each(($add) =>
-          {
-            if ($add.is(":visible"))
-            {
-              cy.wrap($add)
-              .click()
-              .click()           
-            }
+          cy.get("input[id='name']")
+          .clear()
+          .type("TEST-SWOT VON MAX")
+          cy.get("textarea[id='description']")
+          .clear()
+          .type("TEST-SWOT ist ein Testscenario von einem API eingeloggten benutzer")
+          
+          cy.intercept('POST', /.*api\/saves.*/).as('save')
+          cy.get('button[type="submit"]')
+          .click()
+          cy.wait("@save")
+          cy.get("@save")
+          .its("response")
+          .should('include',{
+              statusCode: 201
+            })
+
+          cy.log("new SWOT created and saved for max@test.test")
+
+        })
+
+        it('trys to fill factors in saved TEST-SWOT VON MAX', () =>{
+          cy.visit("/")  
+          cy.loginViaApi("max@test.test", "password")
+          cy.visit("/swot-analysis")
+          cy.url().should("include","swot-analysis")
+          
+          cy.contains("TEST-SWOT VON MAX")
+            .click()
+
+          cy.intercept('GET', /.*api\/saves.*/).as('loadSave')
+          cy.wait("@loadSave")
+          cy.get("@loadSave")
+          .its("response")
+          .should('include',{
+              statusCode: 200
           })
-        
+          
+          cy.url().should("include", "swot-analysis")
+          cy.log("Save loaded")
+
+          //
+          //Karten werden hinzugefügt und mit Testdaten gefüllt
+          //Es werden erst alle Accordions zusammengeklappt!
+          cy.get("button[class='accordion-button']")
+          .click()
+          
+          //Gehe schritt für Schritt die Accordions durch und fügen Inputfelder hinzu bis wir 4 erreicht haben
+          //Dann werden diese mit hilfe von FillFactors mit Testdaten aufgefüllt
           cy.get("button[class='accordion-button collapsed']")
           .each(($press) =>
           {
@@ -100,7 +118,8 @@ describe('SWOT Analyisis', () => {
               cy.wrap($press)
               .click()
             }
-            cy.wait(500)
+            cy.wait(200)
+            
             cy.get('div[class="addCard card"]')
             .each(($add) =>
             {
@@ -108,32 +127,26 @@ describe('SWOT Analyisis', () => {
               {
                 cy.wrap($add)
                 .click()
-                .click()     
-              }     
+                .click()
+                //Fügt daten hinzu
+                FillFactors();         
+              }
+            
             })
-          })
+           })
+          
+            cy.log("Additional Cards added and filled")
 
-          cy.log("Karten hinzugefügt")
+            cy.intercept('POST', /.*api\/saves.*/).as('save')
+            cy.contains("Speichern")
+              .click();
+            cy.get("@save")
+            .its("response")
+            .should('include',{
+                statusCode: 200
+              })
 
-          cy.log("Starting filling up Fields")
-          FillFactors();
-
-          //TODO other wait option
-          cy.wait(500)
-          cy.get("button[class='accordion-button collapsed']")
-          .each(($press) =>
-          {
-            if ($press.is(":visible"))
-            {
-              cy.wrap($press)
-              .click()
-            }
-            //TODO other wait option
-            cy.wait(500)
-            FillFactors();
-
-          })
-    })
+      })
 })
 
 function FillFactors()
@@ -154,9 +167,13 @@ function FillFactors()
             let result = sampleText.match(regex);
 
             cy.wrap($input)
+            .clear()
             .type(swot[result[1]][result[2]][key]);
 
+            CheckFactor(key,$input,swot,result);
+
           }
+
         } 
       }
   }
@@ -171,5 +188,138 @@ function FillFactors()
   cy.get("textArea")
     .each(GetFillFunction("desc", swot))
   })
-  
+
+  function CheckFactor(key, $input, swot, result,)
+  {
+   
+    switch(key)
+      {
+        case "title": 
+          switch(result[1])
+          {
+            case "strengths":
+              switch(result[2])
+                {
+                case "0":cy.wrap($input).should('have.value', swot["strengths"][0]["title"])     
+                      break;
+                case "1":cy.wrap($input).should('have.value', swot["strengths"][1]["title"])
+                      break;
+                case "2":cy.wrap($input).should('have.value', swot["strengths"][2]["title"])
+                      break;
+                case "3":cy.wrap($input).should('have.value', swot["strengths"][3]["title"])
+                      break;
+                }
+            break;
+
+            case "weaknesses": 
+              switch(result[2])
+                {
+                case "0":cy.wrap($input).should('have.value', swot["weaknesses"][0]["title"])
+                      break;
+                case "1":cy.wrap($input).should('have.value', swot["weaknesses"][1]["title"])
+                      break;
+                case "2":cy.wrap($input).should('have.value', swot["weaknesses"][2]["title"])
+                      break;
+                case "3":cy.wrap($input).should('have.value', swot["weaknesses"][3]["title"])
+                      break;
+                }
+              break;
+            
+            case "chances": 
+              switch(result[2])
+                {
+                case "0":cy.wrap($input).should('have.value', swot["chances"][0]["title"])
+                      break;
+                case "1":cy.wrap($input).should('have.value', swot["chances"][1]["title"])
+                      break;
+                case "2":cy.wrap($input).should('have.value', swot["chances"][2]["title"])
+                      break;
+                case "3":cy.wrap($input).should('have.value', swot["chances"][3]["title"])
+                      break;
+                }
+              break;
+
+            case "risks": 
+              switch(result[2])
+                {
+                case "0":cy.wrap($input).should('have.value', swot["risks"][0]["title"])
+                      break;
+                case "1":cy.wrap($input).should('have.value', swot["risks"][1]["title"])
+                      break;
+                case "2":cy.wrap($input).should('have.value', swot["risks"][2]["title"])
+                      break;
+                case "3":cy.wrap($input).should('have.value', swot["risks"][3]["title"])
+                      break;
+                }
+              break;
+          } 
+        break;
+
+        case "desc" : 
+          switch(result[1])
+          {
+            case "strengths": 
+              switch(result[2])
+                {
+                case "0":cy.wrap($input).should('have.value', swot["strengths"][0]["desc"])
+                      break;
+                case "1":cy.wrap($input).should('have.value', swot["strengths"][1]["desc"])
+                      break;
+                case "2":cy.wrap($input).should('have.value', swot["strengths"][2]["desc"])
+                      break;
+                case "3":cy.wrap($input).should('have.value', swot["strengths"][3]["desc"])
+                      break;
+                }
+            break;
+
+            case "weaknesses": 
+              switch(result[2])
+                {
+                case "0":cy.wrap($input).should('have.value', swot["weaknesses"][0]["desc"])
+                      break;
+                case "1":cy.wrap($input).should('have.value', swot["weaknesses"][1]["desc"])
+                      break;
+                case "2":cy.wrap($input).should('have.value', swot["weaknesses"][2]["desc"])
+                      break;
+                case "3":cy.wrap($input).should('have.value', swot["weaknesses"][3]["desc"])
+                      break;
+                }
+              break;
+            
+            case "chances": 
+              switch(result[2])
+                {
+                case "0":cy.wrap($input).should('have.value', swot["chances"][0]["desc"])
+                      break;
+                case "1":cy.wrap($input).should('have.value', swot["chances"][1]["desc"])
+                      break;
+                case "2":cy.wrap($input).should('have.value', swot["chances"][2]["desc"])
+                      break;
+                case "3":cy.wrap($input).should('have.value', swot["chances"][3]["desc"])
+                      break;
+                }
+              break;
+              
+            case "risks": 
+              switch(result[2])
+                {
+                case "0":cy.wrap($input).should('have.value', swot["risks"][0]["desc"])
+                      break;
+                case "1":cy.wrap($input).should('have.value', swot["risks"][1]["desc"])
+                      break;
+                case "2":cy.wrap($input).should('have.value', swot["risks"][2]["desc"])
+                      break;
+                case "3":cy.wrap($input).should('have.value', swot["risks"][3]["desc"])
+                      break;
+                }
+              break;
+          } 
+        break;
+         
+      }
+   
+  }
 }
+     
+   
+
