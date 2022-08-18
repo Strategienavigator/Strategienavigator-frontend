a8c040b2b55b863400e31a0690a27312dda66c11
Nichlas Schipper <Nichlas12323@gmail.com>
2022-08-19 01:55:38 +0200

    updated react-scripts
    Co-Authored-By: Marco Janssen <22887392+ma1160@users.noreply.github.com>

--- src/general-components/Tool/FormComponent/FormComponent.tsx
+++ src/general-components/Tool/FormComponent/FormComponent.tsx
@@ -1,6 +1,5 @@
 import {Component} from "react";
 import {Form} from "react-bootstrap";
-import {randomBytes} from "crypto";
 
 
 export interface FormComponentProps {
@@ -15,8 +14,6 @@ export interface FormComponentProps {
 }
 
 export abstract class FormComponent<P extends FormComponentProps, S> extends Component<P, S> {
-    private key: string = randomBytes(200).toString();
-
     protected constructor(props: Readonly<P> | P);
     protected constructor(props: P, context: any);
     protected constructor(props: P | Readonly<P>, context?: any) {
@@ -25,14 +22,13 @@ export abstract class FormComponent<P extends FormComponentProps, S> extends Com
 
     public render = () => {
         return (
-            <Form key={this.key} aria-disabled={this.props.disabled} name={this.props.id}
+            <Form key={this.props.id} aria-disabled={this.props.disabled} name={this.props.id}
                   id={this.props.id}>
 
                 {this.build()}
             </Form>
         );
     }
-
 
     protected abstract build(): JSX.Element;
 
