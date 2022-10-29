eac2054d09378276b8e289b06b257ebc3c078a5b
Geist5000 <geist5000@gmail.com>
2022-10-30 01:13:02 +0200

    changed test so it can succeed

--- cypress/e2e/validation/registration.cy.js
+++ cypress/e2e/validation/registration.cy.js
@@ -1,61 +1,65 @@
 describe('register on the Website', () => {
-    beforeEach(() =>{
-        cy.task("queryDb",`DELETE FROM toolbox.users WHERE username="Tim_Tester";`);
+    beforeEach(() => {
+        cy.task("queryDb", `DELETE FROM toolbox.users WHERE username="Tim_Tester";`);
     })
-    it('trys to send a email',() =>{
+    it('trys to send a email', () => {
         cy.visit("/register");
         cy.intercept('GET', /.*api\/checkEmail.*/).as('checkEmail')
         cy.get('#email').type('Tim@jade-hs.de')
         cy.wait("@checkEmail")
 
         cy.get('@checkEmail')
-        .its("response.body")
-        .should('nested.include',{"data.available": true
-        })
+            .its("response.body")
+            .should('nested.include', {
+                "data.available": true
+            })
         cy
-        .contains("E-Mail ist Verfügbar!")
-        .should("be.visible")
+            .contains("E-Mail ist Verfügbar!")
+            .should("be.visible")
 
         //--
         cy.intercept('GET', /.*api\/checkUsername.*/).as('checkUser')
         cy.get('#username').type('Tim_Tester')
-       
+
         cy.wait("@checkUser")
-        
+
         cy.get('@checkUser')
-        .its("response.body")
-        .should('nested.include',{"data.available": true
-        })
+            .its("response.body")
+            .should('nested.include', {
+                "data.available": true
+            })
         cy
-        .contains("Username ist Verfügbar!")
-        .should("be.visible")
+            .contains("Username ist Verfügbar!")
+            .should("be.visible")
 
         //--
         cy.get('#password').type('Password123*')
         cy.get('#passwordConfirm').type('Password123*')
         cy.contains("Ihr Passwort ist gültig!")
-        .should("be.visible")
+            .should("be.visible")
+
+        cy.get("input[name=\"captcha\"]").type("test_input"); // input doesn't matter because check is disabled
 
         cy.intercept('POST', /.*api\/users.*/).as('create')
         cy.get('button[type=submit]').click()
 
         cy.get(".feedback.SUCCESS")
-        .contains("Username ist Verfügbar!")
-        .should("be.visible")
+            .contains("Username ist Verfügbar!")
+            .should("be.visible")
 
         cy.log('**register API call**')
         cy.wait('@create')
-        .its('request.body')
-        .should('include',"Tim_Tester")
-        .and("include","Tim@jade-hs.de")
+            .its('request.body')
+            .should('include', "Tim_Tester")
+            .and("include", "Tim@jade-hs.de")
 
         cy.get('@create').its('response')
-        .should('include',{
-            statusCode: 201
-          })
+            .should('include', {
+                statusCode: 201
+            })
 
         cy
-        .contains("Konto erstellt! Überprüfe deine Emails!")
-        .should("be.visible")
+            .contains("Konto erstellt! Überprüfe deine Emails!")
+            .should("be.visible")
     })
 })
