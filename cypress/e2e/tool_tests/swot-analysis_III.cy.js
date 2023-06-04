36d05c3bc24bfc732ad2cb737411564bcf4cc49a
Geist5000 <geist5000@gmail.com>
2023-06-04 17:28:57 +0200

    fixed step 3 swot test

--- cypress/e2e/tool_tests/swot-analysis_III.cy.js
+++ cypress/e2e/tool_tests/swot-analysis_III.cy.js
@@ -1,28 +1,28 @@
 describe('SWOT Analyisis Part III', () => {
     beforeEach(() => {
         cy.task("queryDb", `DELETE FROM toolbox.saves WHERE owner_id= 1 AND name= "TEST-SWOT VON MAX";`);
-        
+
     })
     it('trys to classify the steps', () => {
         cy.CreateSave("swot-2", "TEST-SWOT VON MAX", 2)
         cy.LoginAndLoad("swot")
-        
+
         cy.get(".addClassification.btn.btn-primary")
         .click()
         cy.wait(100)
         cy.get("input[name='droppable-1']")
         .clear()
         .type("Klassifikation 1")
-        
+
         cy.get(".addClassification.btn.btn-primary")
         .click()
         cy.wait(100)
         cy.get("input[name='droppable-2']")
         .clear()
         .type("Klassifikation 2")
-        
-        
-        //collapse accordion to hide 
+
+
+        //collapse accordion to hide
         cy.get(".accordion-button")
         .each(($button) => {
             if ($button.is(":visible")) {
@@ -33,14 +33,14 @@ describe('SWOT Analyisis Part III', () => {
         let MAX_SUM_STEPS = 20
         for (let i = 0; i < MAX_SUM_STEPS; i++) {
             cy.get("form[id='swot-classify-alternate-actions']>.actionCards")
-            .find(".btn.btn-primary.btn-sm").as("button")
-            .first()
+            .find(".btn.btn-primary.btn-sm")
+            .first().as("button")
             .click()
-            
+
             let select
             let inputValue
             let valueText
-            
+
             //To get a specific child and the text out of it.
             cy.get("@button")
             .parent()
@@ -50,22 +50,23 @@ describe('SWOT Analyisis Part III', () => {
             .first()
             .invoke("text")
             .as("text")
-            
+
             if (i % 2) {
                 select = 'droppable-1'
                 inputValue = "input[value='Klassifikation 1']"
                 valueText = 'Klassifikation 1'
-                
+
             } else {
                 select = 'droppable-2'
                 inputValue = "input[value='Klassifikation 2']"
                 valueText = 'Klassifikation 2'
             }
-            cy.get('.modal-body select').select(select)
-            
+            cy.get('@button').parent().parent().parent().parent().parent().find(".menu select").select(select)
+
             cy.get('@text').then((text) => {
                 //going through the tree to find current parents and being part of the right input group
                 cy.contains(text)
+                .parent()
                 .parent()
                 .parent()
                 .parent()
@@ -79,7 +80,7 @@ describe('SWOT Analyisis Part III', () => {
                 .find("input")
                 .should("contain.value", valueText)
             })
-            
+
         }
         cy.contains("Speichern")
         .click();
