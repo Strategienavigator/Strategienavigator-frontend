83ad44bdd95fa329bbfbecf488d99844f69df83a
Geist5000 <geist5000@gmail.com>
2024-03-11 22:40:49 +0100

    added test of currently misbehaving feature

--- src/components/platform/my-profile/MyProfile.cy.tsx
+++ src/components/platform/my-profile/MyProfile.cy.tsx
@@ -65,11 +65,11 @@ describe('MyProfile Page', () => {
         cy.get('@ownedCount').should('have.text', 1);
         cy.get('@sharedCount').should('have.text', 2);
 
-        cy.get('#username').should('have.value', "TestUser").should('have.attr',"readonly");
-        cy.get('#email').should('have.value', "max@test.test").should('have.attr',"readonly");
+        cy.get('#username').should('have.value', "TestUser").should('have.attr', "readonly");
+        cy.get('#email').should('have.value', "max@test.test").should('have.attr', "readonly");
     });
 
-    it("Check edit button and page", function(){
+    it("Check edit button and page", function () {
         mount(<UserContextComponent><MemoryRouter><MyProfile></MyProfile></MemoryRouter></UserContextComponent>);
 
         cy.contains('Bearbeiten').click();
@@ -77,33 +77,48 @@ describe('MyProfile Page', () => {
         cy.contains('Änderungen speichern');
         cy.contains('Benutzer löschen');
 
-        cy.get('#username').should('have.value', "TestUser").should('not.have.attr',"readonly");
-        cy.get('#email').should('have.value', "max@test.test").should('not.have.attr',"readonly");
+        cy.get('#username').should('have.value', "TestUser").should('not.have.attr', "readonly");
+        cy.get('#email').should('have.value', "max@test.test").should('not.have.attr', "readonly");
 
-        cy.get('#new_password').type("pass").should('have.value','pass');
-        cy.get('#new_password_confirm').clear().type("pass").should('have.value','pass');
+        cy.get('#new_password').type("pass").should('have.value', 'pass');
+        cy.get('#new_password_confirm').clear().type("pass").should('have.value', 'pass');
 
-        cy.get('#username').clear().type("newUsername").should('have.value','newUsername');
-        cy.get('#email').clear().type("newUsername").should('have.value','newUsername');
+        cy.get('#username').clear().type("newUsername").should('have.value', 'newUsername');
+        cy.get('#email').clear().type("newUsername").should('have.value', 'newUsername');
     });
 
-    it("Check back button", function() {
+    it("Check back button", function () {
         mount(<UserContextComponent><MemoryRouter><MyProfile></MyProfile></MemoryRouter></UserContextComponent>);
-        cy.get('#username').should('have.value', "TestUser").should('have.attr',"readonly");
+        cy.get('#username').should('have.value', "TestUser").should('have.attr', "readonly");
 
         cy.contains('Bearbeiten').click();
 
-        cy.get('#username').should('have.value', "TestUser").should('not.have.attr',"readonly");
+        cy.get('#username').should('have.value', "TestUser").should('not.have.attr', "readonly");
 
 
-        cy.get('#username').clear().type("newUsername").should('have.value','newUsername');
-        cy.get('#email').clear().type("newUsername").should('have.value','newUsername');
+        cy.get('#username').clear().type("newUsername").should('have.value', 'newUsername');
+        cy.get('#email').clear().type("newUsername").should('have.value', 'newUsername');
 
-        cy.contains('zurück',{matchCase:false}).click();
+        cy.contains('zurück', {matchCase: false}).click();
 
-        cy.get('#username').should('have.value', "TestUser").should('have.attr',"readonly");
-        cy.get('#email').should('have.value','max@test.test').should('have.attr',"readonly");
+        cy.get('#username').should('have.value', "TestUser").should('have.attr', "readonly");
+        cy.get('#email').should('have.value', 'max@test.test').should('have.attr', "readonly");
 
+        cy.get('#new_password').should('not.exist');
+        cy.get('#new_password_confirm').should('not.exist');
+    });
+
+    it("Test same and different Password", function () {
+        mount(<UserContextComponent><MemoryRouter><MyProfile></MyProfile></MemoryRouter></UserContextComponent>);
+
+        cy.contains('Bearbeiten').click();
+
+        cy.get('#new_password').type('StrongPassw0rd!');
+        cy.get('#new_password_confirm').type('DifferentStrongPassw0rd!');
+
+        cy.get('#current_password').type("CurrentPass"); // only for form validation not to fail
+
+        cy.contains('Passwörter müssen übereinstimmen');
     })
 });
 
